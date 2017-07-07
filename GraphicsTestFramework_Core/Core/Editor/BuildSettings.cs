974d3bf195f6260ee8f8d99a60a48df8f583f851
Matt Dean <matthewdean3d@gmail.com>
2017-07-07 15:26:14 +0100

    Added full automation mode and refactor SuiteList
    - Added BuildSetting script to handle prebuild
    - Added global scripting defines list
    - Added support for custom scripting defines
    - Moved Prepare Build methods
    - Added command line argument support
    - Added full automation mode (ignore menus / "-automation true")
    - Refactored "SuiteList" into "Settings" objects

---
+++ GraphicsTestFramework_Core/Core/Editor/BuildSettings.cs
@@ -0,0 +1,68 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+namespace GraphicsTestFramework
+{
+    public class BuildSettings : EditorWindow
+    {
+        // Scripting defines for the core
+        static string[] coreScriptingDefines = new string[1] 
+        {
+            "UTF_EXISTS"
+        };
+
+        // Menu Item
+        [MenuItem("RuntimeTestFramework/BuildSettings")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(BuildSettings)); // Get window
+        }
+
+        // GUI
+        void OnGUI()
+        {
+            GUILayout.Label("Update Suite Information", EditorStyles.boldLabel); // Label
+            if (GUILayout.Button("Build Suite List")) // If button
+                PrepareBuild(); // Prepare build
+            if (GUILayout.Button("Build Debug Suite List")) // If button
+                PrepareDebugBuild(); // Prepare debug build
+        }
+
+        // Setup for build
+        public static void PrepareBuild()
+        {
+            SuiteManager.GenerateSceneList(false); // Create suite structure
+            int platformCount = Enum.GetNames(typeof(BuildTargetGroup)).Length; // Get platform count
+            for (int i = 0; i < platformCount; i++) // Iterate all platforms
+                PlayerSettings.SetApplicationIdentifier((BuildTargetGroup)i, "com.UnityTechnologies.RuntimeTestFramework"); // Set bundle identifiers
+            SetScriptingDefines(); // Set defines
+            PlayerSettings.bundleVersion = Common.applicationVersion; // Set application version
+        }
+
+        // Setup for debug build
+        public static void PrepareDebugBuild()
+        {
+            SuiteManager.GenerateSceneList(true); // Create suite structure
+            int platformCount = Enum.GetNames(typeof(BuildTargetGroup)).Length; // Get platform count
+            for (int i = 0; i < platformCount; i++) // Iterate all platforms
+                PlayerSettings.SetApplicationIdentifier((BuildTargetGroup)i, "com.UnityTechnologies.RuntimeTestFramework"); // Set bundle identifiers
+            SetScriptingDefines(); // Set defines
+            PlayerSettings.bundleVersion = Common.applicationVersion; // Set application version
+        }
+
+        // Set scripting define symbols
+        static void SetScriptingDefines()
+        {
+            Settings settings = SuiteManager.GetSettings(); // Get settings
+            string output = ""; // Create output string
+            for (int i = 0; i < coreScriptingDefines.Length; i++) // Iterate core defines
+                output += coreScriptingDefines[i] + ";"; // Add
+            for (int i = 0; i < settings.scriptingDefines.Length; i++) // Iterate settings defines
+                output += settings.scriptingDefines[i] + ";"; // Add
+            int platformCount = Enum.GetNames(typeof(BuildTargetGroup)).Length; // Get platform count
+            for (int i = 0; i < platformCount; i++) // Iterate all platforms
+                PlayerSettings.SetScriptingDefineSymbolsForGroup((BuildTargetGroup)i, output); // Add custom to current
+        }
+    }
+}
